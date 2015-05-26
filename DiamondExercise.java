7c7318b4452e609e72c012c7fdfa16e95ae2b739
fanfal <271299626@qq.com>
2015-05-26 17:45:10 +0800

    [xuhao]:finish the DimondExercise

--- DiamondExercise.java
+++ DiamondExercise.java
@@ -2,7 +2,8 @@ public class DiamondExercise
 {
   public static void main(String[] args)
   {
-   isoscelesTriangle(10);
+  //isoscelesTriangle(10);
+   diamond(11);
   }
   public static void isoscelesTriangle(int n)
   {
@@ -18,5 +19,44 @@ public class DiamondExercise
       }
       System.out.println("\n");
     }
+  }
+
+  public static void diamond(int n)
+  {
+    if(n%2 == 0)
+    {
+      System.out.print("the input is error,please check");
+      return;
+    }
+
+    int halfLine = (n+1)/2;
+    for(int row=1;row<=n;row++)
+    {
+      if(row <= halfLine)
+      {
+        for(int j=1;j<=halfLine-row;j++)
+        {
+         System.out.print(" ");
+        }
+        for(int k=1;k<=2*row-1;k++)
+        {
+          System.out.print("*");
+        }
+        System.out.println("\n");
+      }
+      else
+      {
+        for(int j=1;j<=row-halfLine;j++)
+        {
+          System.out.print(" ");
+        }
+        for(int k=1;k<=2*(2*halfLine-row)-1;k++)
+        {
+          System.out.print("*");
+        }
+        System.out.println("\n");
+      }
+    }
+
   }
 }
